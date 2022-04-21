df209cf3af1aa8beeb4592f40bbb83f569acaf5d
Gaston <gianni.gaston@hotmail.com>
2022-04-20 21:29:58 -0300

    ahora se ve lo que seleccionaste

--- js/app.js
+++ js/app.js
@@ -4,19 +4,36 @@ let piedra = document.querySelector('.piedra');
 let papel = document.querySelector('.papel');
 let tijeras = document.querySelector('.tijeras');
 
+const playerOptions = [piedra,papel,tijeras];
+const computerOptions = ['Piedra','Papel','Tijeras']
+
 btn.addEventListener('click', startGame);
-piedra.addEventListener('click', choise);
-papel.addEventListener('click', choise);
-tijeras.addEventListener('click', choise);
+piedra.addEventListener('click', choisePiedra);
+papel.addEventListener('click', choisePapel);
+tijeras.addEventListener('click', choiseTijeras);
+
+
 
 function startGame() {
     btn = document.querySelector('.btn-header').style = 'display: none';
     main = document.querySelector('.main').style = 'display:flex'
 }
 
-function choise(item){
-    
+function choisePiedra(item){
+    resultado = document.querySelector('.resultado');
+    piedra.textContent=('Piedra')
+    resultado.textContent=(`Elegiste ${piedra.textContent}`);
 }
+function choisePapel(item){
+    resultado = document.querySelector('.resultado');
+    resultado.textContent=('Elegiste Papel!')
+}
+function choiseTijeras(item){
+    resultado = document.querySelector('.resultado');
+    resultado.textContent=('Elegiste Tijeras!')
+}
+
+
 
 
 
