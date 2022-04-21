0199ff5d897a299d659a342da93010c6bc0c20b0
Gaston <gianni.gaston@hotmail.com>
2022-04-21 15:17:08 -0300

    el oponente ya elige de forma aleatoria

--- js/app.js
+++ js/app.js
@@ -3,15 +3,13 @@ let main = document.querySelector('.main');
 let piedra = document.querySelector('.container-piedra');
 let papel = document.querySelector('.container-papel');
 let tijeras = document.querySelector('.container-tijeras');
+let vidas = 5
 
-const playerOptions = [piedra,papel,tijeras];
-const computerOptions = ['Piedra','Papel','Tijeras']
 
 btn.addEventListener('click', startGame);
-piedra.addEventListener('click', choisePiedra);
-papel.addEventListener('click', choisePapel);
-tijeras.addEventListener('click', choiseTijeras);
-
+piedra.addEventListener('click', choisePiedra,);
+papel.addEventListener('click', choisePapel,);
+tijeras.addEventListener('click', choiseTijeras,);
 
 
 function startGame() {
@@ -19,19 +17,47 @@ function startGame() {
     main = document.querySelector('.main').style = 'display:flex'
 }
 
-function choisePiedra(item){
+function choisePiedra() {
     resultado = document.querySelector('.resultado');
+    resultado.textContent = (`Elegiste Piedra`);
+    main = document.querySelector('.main').style = 'display:none'
+    computerSelection()
+}
+
+function choisePapel() {
+    resultado = document.querySelector('.resultado');
+    resultado.textContent = ('Elegiste Papel!')
+    main = document.querySelector('.main').style = 'display:none'
+    computerSelection()
+}
+
+function choiseTijeras() {
+    resultado = document.querySelector('.resultado');
+    resultado.textContent = ('Elegiste Tijeras!')
+    main = document.querySelector('.main').style = 'display:none';
+    computerSelection()
+}
+
+const playerOptions = [piedra, papel, tijeras];
+
+function computerSelection(result) {
+    let resultadoOponente = document.querySelector('.resultado-oponente');
+    result = Math.floor(Math.random() * 3) + 1;
+    switch (result) {
+        case 1: return result= 'piedra', resultadoOponente.textContent=(`Tu oponente eligío ${result}`)
+        case 2: return result= 'Tijeras', resultadoOponente.textContent=(`Tu oponente eligío ${result}`)
+        default: return result= 'Papel', resultadoOponente.textContent=(`Tu oponente eligío ${result}`)
+     } 
     
-    resultado.textContent=(`Elegiste Piedra`);
 }
-function choisePapel(item){
-    resultado = document.querySelector('.resultado');
-    resultado.textContent=('Elegiste Papel!')
-}
-function choiseTijeras(item){
-    resultado = document.querySelector('.resultado');
-    resultado.textContent=('Elegiste Tijeras!')
-}
+
+
+
+
+
+
+        
+
 
 
 
